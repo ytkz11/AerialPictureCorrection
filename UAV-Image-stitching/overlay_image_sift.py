a317b606801ffa1e6ba36703216135a5a1162004
ytkz11 <ytkz11@163.com>
2024-08-07 14:51:33 +0800

    在重叠区域进行sift

--- UAV-Image-stitching/overlay_image_sift.py
+++ UAV-Image-stitching/overlay_image_sift.py
@@ -9,39 +9,91 @@ from overlay_image_areas import same_area
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-def match_3d(d1, d2):
-  '''
-  使两个矩阵尺寸相等
-  :param d1:
-  :param d2:
-  :return:
-  '''
-  x1, y1, z1 = np.shape(d1)
-  x2, y2, z2 = np.shape(d2)
+import random
+import math
+import PIL.Image as Image
 
-  if x1 > x2 and y1 > y2:
-    d3 = np.zeros((x1, y1, 3))
-    for i in range(z1):
-      print(i)
-      d3[5:5 + x2, 5:5 + y2, i] = d2[:, :, i]
-  else:
+def ransac(pts1, pts2):
+    best_inlinenums = 0
+    best_f = np.zeros([3, 3])
+    best_distance = []
+    i = 0
+    while i < 25:
+        # 随机选择8个点
+        index = set()
+        while len(index) < 8:
+            index.add(random.randrange(len(pts1)))
+        # 根据这8个点生成矩阵
+        a = np.zeros([8, 9])
+        for j, item in enumerate(index):
+            (x1, y1) = pts1[item]
+            (x2, y2) = pts2[item]
+            a[j][0] = x1 * x2
+            a[j][1] = x2 * y1
+            a[j][2] = x2
+            a[j][3] = x1 * y2
+            a[j][4] = y1 * y2
+            a[j][5] = y2
+            a[j][6] = x1
+            a[j][7] = y1
+            a[j][8] = 1
+        u, d, vt = np.linalg.svd(a)
+        f = vt[8]
+        f = f.reshape(3, 3)
+        # 根据F计算内点数，首先计算极线
+        one = np.ones(len(pts1))
+        pts1_new = np.insert(pts1, 2, values=one, axis=1)  # 构造齐次坐标系
+        elines = [np.dot(f, pts1_new[i]) for i in range(len(pts1))]  # 极线
+        # 计算pts2中每一个点到对应极线的距离
+        pts2_new = np.insert(pts2, 2, values=one, axis=1)  # 构造齐次坐标系
+        distances = []
+        inline_num = 0
+        for pt, l in zip(pts2_new, elines):
+            div = abs(np.dot(np.transpose(pt), l))
+            dived = math.sqrt(l[0] * l[0] + l[1] * l[1])
+            d = div / dived
+            if d <= 0.25:
+                # if d <= 3:
 
-    if y1 < y2:
-      d3 = np.zeros((x2, y2, 3))
+                inline_num = inline_num + 1
+            distances.append(d)
+        if inline_num > best_inlinenums:
+            best_f = f[:]
+            best_inlinenums = inline_num
+            best_distance = distances[:]
+        i += 1
+    return best_f, best_distance
 
-      for i in range(z1):
-        d3[x2-x1:x2-x1 + x1, y2-y1:y2-y1 + y1, i] = d1[:, :, i]
 
-  d = []
-  x3, y3, z3 = np.shape(d3)
-  datagray = np.zeros(shape=(x3, y3, z3, 2))
-  datagray[:, :, :, 0] = d3
-  if x3 == x1:
-    datagray[:, :, :, 1] = d1
-  elif x3 == x2:
-    datagray[:, :, :, 1] = d2
+def match_3d( d1, d2):
+    x1, y1, z1 = np.shape(d1)
+    x2, y2, z2 = np.shape(d2)
 
-  return datagray
+    if x1 > x2 and y1 > y2:
+        d3 = np.zeros((x1, y1, 3)).astype('uint8')
+        for i in range(z1):
+            print(i)
+            d3[1:1 + x2, 1:1 + y2, i] = d2[:, :, i]
+    else:
+
+        if y1 < y2:
+            d3 = np.zeros((x2, y2, 3))
+
+            for i in range(z1):
+                d3[1:1 + x1, 1:1 + y1, i] = d1[:, :, i]
+
+    d = []
+    x3, y3, z3 = np.shape(d3)
+    datagray = np.zeros(shape=(x3, y3 * 2, z3)).astype('uint8')
+    datagray[:, 0:y3, :] = d3
+    if x3 == x1 and y3 == y1:
+        datagray[:, y3:y3 * 2, :] = d1
+    elif x3 == x2 and y3 == y2:
+        datagray[:, y3:y3 * 2, :] = d2
+
+    datagray[datagray == 0] = 255
+
+    return np.array(datagray).astype('uint8')
 def sift(img1,img2):
     sift = cv2.xfeatures2d.SIFT_create()
     kp1, des1 = sift.detectAndCompute(img1, None)  # des是描述子
@@ -60,32 +112,125 @@ def sift(img1,img2):
 
     matches = flann.knnMatch(des1, des2, k=2)
 
-    # Need to draw only good matches, so create a mask
-    matchesMask = [[0, 0] for i in range(len(matches))]
+    good = []  # 较好的匹配
+    pts1 = []  # img1中较好的匹配的坐标
+    pts2 = []
 
-    # ratio test as per Lowe's paper
     for i, (m, n) in enumerate(matches):
         if m.distance < 0.7 * n.distance:
+            good.append(m)
+            pts1.append(kp1[m.queryIdx].pt)
+            pts2.append(kp2[m.trainIdx].pt)
+
+    best_f, distances = ransac(pts1, pts2)
+    matchesMask = [[0, 0] for i in range(len(good))]
+    ransac_good = []
+    for i, k in enumerate(distances):
+        if k <= 1:
             matchesMask[i] = [1, 0]
 
-    draw_params = dict(matchColor=(0, 255, 0),
-                       singlePointColor=(255, 0, 0),
-                       matchesMask=matchesMask,
-                       flags=cv2.DrawMatchesFlags_DEFAULT)
+            ransac_good.append([good[i]])
 
-    img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
+
+
+
+    # ratio test as per Lowe's paper
+    # for i, (m, n) in enumerate(matches):
+    #     if m.distance < 0.6 * n.distance:
+    #         matchesMask[i] = [1, 0]
+
+    draw_params = dict(matchColor=(0, 255, 0),
+                       # singlePointColor=(255, 0, 0),
+                       # matchesMask=matchesMask,
+                       flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, ransac_good, None, **draw_params)
 
     plt.imshow(img3, ), plt.show()
-    cv2.imwrite( 'overlay_image_sift_test.jpg', img3)
+    cv2.imwrite( 'overlay_image_sift_test2.jpg', img3)
+    merge_image(img1,img2,kp1,kp2,ransac_good)
+    print()
+def warpImages(img1, img2, M):
+
+    h,w,z = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+    # 检查变换后的坐标
+    print("Transformed coordinates dst:")
+    print(dst)
+
+    # 确保变换后的坐标合理
+    if np.any(dst < 0) or np.any(dst > np.array([img2.shape[1], img2.shape[0]])):
+        print("Some points are outside the image boundaries.")
+    else:
+        print("All points are within the image boundaries.")
+
+    # 如果有必要，扩展目标图像
+    min_x = int(np.min(dst[:, :, 0]))
+    max_x = int(np.max(dst[:, :, 0]))
+    min_y = int(np.min(dst[:, :, 1]))
+    max_y = int(np.max(dst[:, :, 1]))
+
+    if min_x < 0 or min_y < 0 or max_x > img2.shape[1] or max_y > img2.shape[0]:
+        # 计算新的宽度和高度
+
+        # 将原图像粘贴到新图像中
+        dx = abs(min_x) if min_x < 0 else 0
+        dy = abs(min_y) if min_y < 0 else 0
+        new_width = max(max_x, abs(min_x)) + dx
+        new_height = max(max_y, abs(min_y)) + dy
+
+        # 创建一个新的空白图像
+        new_img = np.zeros((new_height, new_width, 3), dtype=np.uint8)
+
+        new_img[dy:dy + img2.shape[0], dx:dx + img2.shape[1]] = img2
 
 
+        # 更新目标图像
+        img2 = new_img
+
+        # 更新变换后的坐标
+        dst[:, :, 0] += dx
+        dst[:, :, 1] += dy
+
+        # 绘制多边形
+        # img2 = cv2.polylines(img2, [np.int32(dst)], True, (255, 0, 0), 3, cv2.LINE_AA)
+
+        # 显示结果图像
+
+        plt.imshow(img2, ), plt.show()
+
+    # 将 img1 按照变换矩阵 M 变换到 img2 的空间
+    warped_img1 = cv2.warpPerspective(img1, M, (img2.shape[1], img2.shape[0]))
+
+    # 将 img1 和 img2 拼接在一起
+    result = cv2.addWeighted(img2, 1, warped_img1, 1, 0)
+    plt.imshow(result, ), plt.show()
+    img = Image.fromarray(result)
+    img.save('merge_result.PNG')
+def merge_image(img1,img2,kp1,kp2,good):
+
+    src_pts = np.float32([kp1[m[0].queryIdx].pt for m in good]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2[m[0].trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+    # Establish a homography
+    M, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+    result = warpImages(img1, img2, M)
+    img = Image.fromarray(result)
+    img.save('merge.PNG')
+
+    plt.imshow(result),plt.show()
 if __name__ == '__main__':
     # 加载两个图像
     raster1 = r'D:\无人机\test\DJI_20230410091557_0118.tif'
     raster2 = r'D:\无人机\test\DJI_20230410091600_0119.tif'
     img1,img2 = same_area(raster1, raster2)
     data2 = match_3d(img1, img2)
-    cv2.imwrite('overlay_image_part_image.jpg', data2)
 
+    data2 = np.array(data2).astype('uint8')
+
+    img = Image.fromarray(data2)
+    img.save('overlay_image_part_image.PNG')
     sift(img1,img2)
 
