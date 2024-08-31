d06c00af8ba7008fa1ff7ec7a511ba1f1d74ca0a
ytkz11 <ytkz11@163.com>
2024-09-01 00:23:29 +0800

    Did not finish writing the program

--- UAV-Image-stitching/two_npy_file_to_stitching.py
+++ UAV-Image-stitching/two_npy_file_to_stitching.py
@@ -5,23 +5,51 @@
 '''
 
 '''
-
-
+import numpy as np
+import json
+import cv2
+import matplotlib.pyplot as plt
 class two_npy_file_to_stitching:
-    def __init__(self, right_img_file, left_img_file):
-        pass
+    def __init__(self, right_img_file, left_img_file, config_file):
+        self.right_img_file = right_img_file
+        self.left_img_file = left_img_file
+        self.config_file = config_file
+        self.cover_part_img_info = self.read_config_file()
 
-    def function(self, nums) -> None:
+        self.read_npy()
+        self.warp()
+    def read_npy(self) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        a = 0
+        # 读取npy
+        self.right_img = np.load(self.right_img_file)
+        self.left_img = np.load(self.left_img_file)
 
+
+        print(self.right_img.shape)
+        print(self.left_img.shape)
+    def warp(self):
+        width = self.right_img.shape[1] + self.left_img.shape[1]
+        height = self.right_img.shape[0] + self.left_img.shape[0]
+
+        H = np.array(self.cover_part_img_info["Homography"])
+        result = cv2.warpPerspective(self.right_img, H, (width, height))
+        result[:self.left_img.shape[0], :self.left_img.shape[1]] = self.left_img
+        plt.imshow(result)
+        plt.show()
+        print()
+    def read_config_file(self):
+        with open(self.config_file, 'r') as f:
+            fcc_data = json.load(f)
+            print(fcc_data)
+        return fcc_data
 
 if __name__ == '__main__':
 
     right_img_file = r'D:\temp\aerial_data\右图的右边.npy'
     left_img_file = r'D:\temp\aerial_data\左图的左边.npy'
-    A = Solution()
-    a = A.function()
+    config_file = r'D:\temp\aerial_data\DJI_20230410091600_0119_DJI_20230410091557_0118.json'
+    A = two_npy_file_to_stitching(right_img_file, left_img_file, config_file)
+
     print()
