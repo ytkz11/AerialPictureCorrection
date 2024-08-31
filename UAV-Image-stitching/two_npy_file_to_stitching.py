926dfb103dc2e2393ae15dda23a6a0ad183a6ea7
ytkz11 <ytkz11@163.com>
2024-08-31 20:22:27 +0800

    Slight alteration

--- UAV-Image-stitching/two_npy_file_to_stitching.py
+++ UAV-Image-stitching/two_npy_file_to_stitching.py
@@ -7,8 +7,8 @@
 '''
 
 
-class Solution:
-    def __init__(self):
+class two_npy_file_to_stitching:
+    def __init__(self, right_img_file, left_img_file):
         pass
 
     def function(self, nums) -> None:
@@ -19,6 +19,9 @@ class Solution:
 
 
 if __name__ == '__main__':
+
+    right_img_file = r'D:\temp\aerial_data\右图的右边.npy'
+    left_img_file = r'D:\temp\aerial_data\左图的左边.npy'
     A = Solution()
     a = A.function()
     print()
